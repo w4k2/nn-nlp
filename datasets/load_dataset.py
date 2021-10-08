dc19cce758050546b00139faa58a9667a84d02a2
jedrzejkozal <jedrzej.kozal@gmail.com>
2021-10-08 15:12:17 +0200

    conda env + LDA tutorial

---
+++ datasets/load_dataset.py
@@ -0,0 +1,17 @@
+import smart_open
+import tarfile
+import re
+
+
+def load_nips():
+    docs = list(extract_nips_documents())
+    return docs
+
+
+def extract_nips_documents(url='https://cs.nyu.edu/~roweis/data/nips12raw_str602.tgz'):
+    with smart_open.open(url, "rb") as file:
+        with tarfile.open(fileobj=file) as tar:
+            for member in tar.getmembers():
+                if member.isfile() and re.search(r'nipstxt/nips\d+/\d+\.txt', member.name):
+                    member_bytes = tar.extractfile(member).read()
+                    yield member_bytes.decode('utf-8', errors='replace')
---
+++ main.py
@@ -0,0 +1,90 @@
+from pprint import pprint
+from gensim.models import LdaModel
+from gensim.corpora import Dictionary
+from gensim.models import Phrases
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import RegexpTokenizer
+
+import datasets
+
+
+def main():
+    docs = datasets.load_dataset.load_nips()
+
+    # Tokenize the documents.
+
+    # Split the documents into tokens.
+    tokenizer = RegexpTokenizer(r'\w+')
+    for idx in range(len(docs)):
+        docs[idx] = docs[idx].lower()  # Convert to lowercase.
+        docs[idx] = tokenizer.tokenize(docs[idx])  # Split into words.
+
+    # Remove numbers, but not words that contain numbers.
+    docs = [[token for token in doc if not token.isnumeric()] for doc in docs]
+
+    # Remove words that are only one character.
+    docs = [[token for token in doc if len(token) > 1] for doc in docs]
+
+    lemmatizer = WordNetLemmatizer()
+    docs = [[lemmatizer.lemmatize(token) for token in doc] for doc in docs]
+
+    # Compute bigrams.
+
+    # Add bigrams and trigrams to docs (only ones that appear 20 times or more).
+    bigram = Phrases(docs, min_count=20)
+    for idx in range(len(docs)):
+        for token in bigram[docs[idx]]:
+            if '_' in token:
+                # Token is a bigram, add to document.
+                docs[idx].append(token)
+
+    # Remove rare and common tokens.
+
+    # Create a dictionary representation of the documents.
+    dictionary = Dictionary(docs)
+
+    # Filter out words that occur less than 20 documents, or more than 50% of the documents.
+    dictionary.filter_extremes(no_below=20, no_above=0.5)
+
+    # Bag-of-words representation of the documents.
+    corpus = [dictionary.doc2bow(doc) for doc in docs]
+
+    print('Number of unique tokens: %d' % len(dictionary))
+    print('Number of documents: %d' % len(corpus))
+
+    # Train LDA model.
+
+    # Set training parameters.
+    num_topics = 10
+    chunksize = 2000
+    passes = 20
+    iterations = 400
+    eval_every = None  # Don't evaluate model perplexity, takes too much time.
+
+    # Make a index to word dictionary.
+    temp = dictionary[0]  # This is only to "load" the dictionary.
+    id2word = dictionary.id2token
+
+    model = LdaModel(
+        corpus=corpus,
+        id2word=id2word,
+        chunksize=chunksize,
+        alpha='auto',
+        eta='auto',
+        iterations=iterations,
+        num_topics=num_topics,
+        passes=passes,
+        eval_every=eval_every
+    )
+
+    top_topics = model.top_topics(corpus)  # , num_words=20)
+
+    # Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
+    avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
+    print('Average topic coherence: %.4f.' % avg_topic_coherence)
+
+    pprint(top_topics)
+
+
+if __name__ == '__main__':
+    main()
