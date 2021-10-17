3d5ce3cb631b6410a1218ee6fe9b14f0e1da9ff8
Michał Leś <michalles3@gmail.com>
2021-10-17 21:14:38 +0200

    changed default device to CPU and enabled multilanguage BERT

--- bert.py
+++ bert.py
@@ -3,6 +3,8 @@ from gensim.corpora import Dictionary, dictionary
 from gensim.models import Phrases
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tokenize import RegexpTokenizer
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
@@ -21,10 +23,10 @@ def extract_features(x_train, y_train, x_test, num_features=100):
 
 def train_bert_model(x_train, y_train, num_topics=100):
     text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
-    # bert_preprocess_url = "https://tfhub.dev/tensorflow/bert_multi_cased_preprocess/3" #multilanguage
-    # bert_model_url = "https://tfhub.dev/tensorflow/bert_multi_cased_L-12_H-768_A-12/4" #multilangauge
-    bert_preprocess_url = "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3" #en_bert
-    bert_model_url = "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1" #en_bert
+    bert_preprocess_url = "https://tfhub.dev/tensorflow/bert_multi_cased_preprocess/3" #multilanguage
+    bert_model_url = "https://tfhub.dev/tensorflow/bert_multi_cased_L-12_H-768_A-12/4" #multilangauge
+    # bert_preprocess_url = "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3" #en_bert
+    # bert_model_url = "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1" #en_bert
     preprocessing_layer = hub.KerasLayer(bert_preprocess_url, name='preprocessing')
     encoder_inputs = preprocessing_layer(text_input)
     encoder = hub.KerasLayer(bert_model_url, trainable=True, name='BERT_encoder')
