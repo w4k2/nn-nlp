3b0cd794c456e3e2a230cf42622b7423d4445861
jedrzejkozal <jedrzej.kozal@gmail.com>
2021-10-26 19:32:27 +0200

    add liar tf-idf features fix

--- tf_idf.py
+++ tf_idf.py
@@ -4,6 +4,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 def extract_features(X_train, X_test, num_features=100):
     tfidf_vectorizer = TfidfVectorizer(max_features=num_features)
     tfidf_vectorizer.fit(X_train)
-    train_features = tfidf_vectorizer.transform(X_train)
-    test_features = tfidf_vectorizer.transform(X_test)
+    train_features = tfidf_vectorizer.transform(X_train).toarray()
+    test_features = tfidf_vectorizer.transform(X_test).toarray()
     return train_features, test_features
