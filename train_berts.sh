c86122341c5b4aa6c27971bc38c129c145898023
jedrzejkozal <jedrzej.kozal@gmail.com>
2021-10-28 12:26:32 +0200

    results for title

--- train_berts.sh
+++ train_berts.sh
@@ -1,8 +1,23 @@
 #! /bin/bash
 
+# python precompute_features.py --dataset_name=esp_fake --attribute=text --extraction_method=lda
+# python precompute_features.py --dataset_name=esp_fake --attribute=text --extraction_method=tf_idf
+# python precompute_features.py --dataset_name=esp_fake --attribute=title --extraction_method=lda
+# python precompute_features.py --dataset_name=esp_fake --attribute=title --extraction_method=tf_idf
 
-python train_bert.py --dataset_name=liar --language=eng
-python train_bert.py --dataset_name=liar --language=multi
+# python precompute_features.py --dataset_name=bs_detector --attribute=title --extraction_method=lda
+# python precompute_features.py --dataset_name=bs_detector --attribute=title --extraction_method=tf_idf
 
-# python train_bert.py --dataset_name=esp_fake --language=eng
-python train_bert.py --dataset_name=esp_fake --language=multi
+python classify.py --dataset_name=esp_fake --attribute=text --extraction_method=lda
+python classify.py --dataset_name=esp_fake --attribute=text --extraction_method=tf_idf
+python classify.py --dataset_name=esp_fake --attribute=title --extraction_method=lda
+python classify.py --dataset_name=esp_fake --attribute=title --extraction_method=tf_idf
+
+python classify.py --dataset_name=bs_detector --attribute=title --extraction_method=lda
+python classify.py --dataset_name=bs_detector --attribute=title --extraction_method=tf_idf
+
+# python train_bert.py --dataset_name=bs_detector --language=eng
+# python train_bert.py --dataset_name=bs_detector --language=multi
+
+# python train_beto.py ## beto is spanish bert --dataset_name=esp_fake --language=spanish
+# python train_bert.py --dataset_name=esp_fake --language=multi
