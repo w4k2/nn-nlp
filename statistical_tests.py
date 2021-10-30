e3cd00d8fd8fa67bac076703fa357a9e0c6693e8
jedrzejkozal <jedrzej.kozal@gmail.com>
2021-10-30 14:06:04 +0200

    separate tables for F-statistic and pvalue

--- statistical_tests.py
+++ statistical_tests.py
@@ -17,13 +17,16 @@ def main():
     avrg_table_bs_detector = get_average_table(results, 'bs_detector')
     pretty_print_table(avrg_table_bs_detector, table_name='bs_detector avrg results')
     utils.save_tex_table.save_tex_table(avrg_table_bs_detector, 'tables/bs_detector_avrg.tex')
+    avrg_table_mixed = get_average_table(results, 'mixed')
+    pretty_print_table(avrg_table_mixed, table_name='mixed avrg results')
+    utils.save_tex_table.save_tex_table(avrg_table_mixed, 'tables/mixed_avrg.tex')
 
-    stats_table_esp = statistical_tests_table(results, 'esp_fake')
-    pretty_print_table(stats_table_esp, table_name='Esp fake F-test')
-    utils.save_tex_table.save_tex_table(stats_table_esp, 'tables/esp_stats.tex')
-    stats_table_bs_detector = statistical_tests_table(results, 'bs_detector')
-    pretty_print_table(stats_table_bs_detector, table_name='bs_detector F-test')
-    utils.save_tex_table.save_tex_table(stats_table_bs_detector, 'tables/bs_detector_stats.tex')
+    for dataset_name in ('esp_fake', 'bs_detector', 'mixed'):
+        stats, pvalue = statistical_tests_table(results, dataset_name)
+        pretty_print_table(stats, table_name=f'{dataset_name} F-test stat')
+        pretty_print_table(pvalue, table_name=f'{dataset_name} F-test pvalue')
+        utils.save_tex_table.save_tex_table(stats, f'tables/{dataset_name}_stats.tex')
+        utils.save_tex_table.save_tex_table(pvalue, f'tables/{dataset_name}_pvalue.tex')
 
 
 def load_results():
@@ -55,24 +58,27 @@ def pretty_print_table(table, table_name=None):
 
 
 def statistical_tests_table(results, dataset_name):
-    selected_results = [(key, value) for key, value in results.items() if key.startswith(dataset_name)]
+    selected_results = [(key[len(dataset_name)+1:], value) for key, value in results.items() if key.startswith(dataset_name)]
 
-    table = [[None]]
+    table_stat = [[None]]
+    table_pvalue = [[None]]
     for i in range(len(selected_results)):
-        row = [selected_results[i][0]]
+        row_stat = [selected_results[i][0]]
+        row_pvalue = [selected_results[i][0]]
         for j in range(len(selected_results)):
-            if len(table[0]) < 2 * len(selected_results):
-                table[0].append(f'{selected_results[j][0]}_stat')
-                table[0].append(f'{selected_results[j][0]}_p-value')
+            if len(table_stat[0]) < len(selected_results)+1:
+                table_stat[0].append(f'{selected_results[j][0]}')
+                table_pvalue[0].append(f'{selected_results[j][0]}')
             if i == j:
-                row.append(None)
-                row.append(None)
+                row_stat.append(None)
+                row_pvalue.append(None)
                 continue
             f, p = cv52cft(selected_results[i][1], selected_results[j][1])
-            row.append(f)
-            row.append(p)
-        table.append(row)
-    return table
+            row_stat.append(f)
+            row_pvalue.append(p)
+        table_stat.append(row_stat)
+        table_pvalue.append(row_pvalue)
+    return table_stat, table_pvalue
 
 
 def cv52cft(a, b):
