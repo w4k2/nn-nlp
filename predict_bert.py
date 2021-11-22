e2c716b531b835433229fc0ac08fdd0c819c792b
jedrzejkozal <jedrzej.kozal@gmail.com>
2021-11-22 14:09:26 +0100

    add precomputing of features with bert

--- predict_bert.py
+++ predict_bert.py
@@ -19,6 +19,7 @@ def main():
     k_fold = sklearn.model_selection.RepeatedStratifiedKFold(n_splits=2, n_repeats=5, random_state=42)
     pbar = tqdm(enumerate(k_fold.split(docs, labels)), desc='Fold feature extraction', total=10)
     for fold_idx, (train_idx, test_idx) in pbar:
+        docs_train = [str(docs[i]) for i in train_idx]
         docs_test = [str(docs[i]) for i in test_idx]
         y_train, y_test = labels[train_idx], labels[test_idx]
         if args.dataset_name == 'mixed':
@@ -30,10 +31,24 @@ def main():
         model = get_model(language=args.language)
         checkpoint_path = f'./weights/bert_{args.language}/{args.dataset_name}/{args.attribute}/fold_{fold_idx}/bert'
         model.load_weights(checkpoint_path)
+
         y_pred = model.predict(docs_test)
         pred_filename = f'./predictions/bert_{args.language}/{args.dataset_name}/{args.attribute}/fold_{fold_idx}/predictions.npy'
         os.makedirs(os.path.dirname(pred_filename), exist_ok=True)
         np.save(pred_filename, y_pred)
+
+        model_features = get_model(language=args.language, add_classifier=False)
+        checkpoint_path = f'./weights/bert_{args.language}/{args.dataset_name}/{args.attribute}/fold_{fold_idx}/bert'
+        model_features.load_weights(checkpoint_path)
+
+        output_path = pathlib.Path(f'extracted_features/{args.dataset_name}_bert_{args.language}')
+        os.makedirs(output_path, exist_ok=True)
+        y_pred = model_features.predict(docs_train)
+        np.save(output_path / f'fold_{fold_idx}_X_train_{args.attribute}.npy', y_pred)
+        np.save(output_path / f'fold_{fold_idx}_y_train_{args.attribute}.npy', y_train)
+        y_pred = model_features.predict(docs_test)
+        np.save(output_path / f'fold_{fold_idx}_X_test_{args.attribute}.npy', y_pred)
+        np.save(output_path / f'fold_{fold_idx}_y_test_{args.attribute}.npy', y_test)
 
 
 def parse_args():
@@ -47,7 +62,7 @@ def parse_args():
     return args
 
 
-def get_model(language='eng'):
+def get_model(language='eng', add_classifier=True):
     preprocess_url_dict = {
         'eng': "https://tfhub.dev/tensorflow/bert_en_cased_preprocess/3",
         'multi': "https://tfhub.dev/tensorflow/bert_multi_cased_preprocess/3"
@@ -58,20 +73,21 @@ def get_model(language='eng'):
     }
     bert_preprocess_url = preprocess_url_dict[language]
     bert_model_url = model_url_dict[language]
-    model = get_keras_model(bert_preprocess_url, bert_model_url)
+    model = get_keras_model(bert_preprocess_url, bert_model_url, add_classifier=add_classifier)
     return model
 
 
-def get_keras_model(bert_preprocess_url, bert_model_url):
+def get_keras_model(bert_preprocess_url, bert_model_url, add_classifier=True):
     text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
     preprocessing_layer = hub.KerasLayer(bert_preprocess_url, name='preprocessing')
     encoder_inputs = preprocessing_layer(text_input)
     encoder = hub.KerasLayer(bert_model_url, trainable=True, name='BERT_encoder')
     outputs = encoder(encoder_inputs)
     out = outputs['pooled_output']
-    out = tf.keras.layers.Dropout(0.1)(out)
-    y = tf.keras.layers.Dense(2, activation='softmax', name='classifier')(out)
-    model = tf.keras.Model(text_input, y)
+    if add_classifier:
+        out = tf.keras.layers.Dropout(0.1)(out)
+        out = tf.keras.layers.Dense(2, activation='softmax', name='classifier')(out)
+    model = tf.keras.Model(text_input, out)
     return model
 
 
