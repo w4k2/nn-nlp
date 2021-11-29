ba3232e1d0c96c6ec4437f59e345da28bbabca42
jedrzejkozal <jedrzej.kozal@gmail.com>
2021-11-29 13:07:56 +0100

    update script for pushing extracted features

--- push_extracted_feautures.sh
+++ push_extracted_feautures.sh
@@ -2,11 +2,17 @@
 
 for dir in `git status | grep "extracted_features/.*"`
 do
+    if [ $dir = "modified:" ]; then
+	continue
+    fi
+    echo "$dir"
     for filename in `ls $dir`
     do
         #echo "$dir/$filename"
-        git add $dir/$filename
+	#echo "$filename"
+        git add $filename
         git commit -m "update extracted features dir $dir file $filename"
         git push
+	#exit
     done
 done
