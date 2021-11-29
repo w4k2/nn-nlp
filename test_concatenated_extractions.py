1c759cf78527700c950a4cbaf1787d273b61a22e
jedrzejkozal <jedrzej.kozal@gmail.com>
2021-11-29 17:42:39 +0100

    add code for concat ensemble

--- test_concatenated_extractions.py
+++ test_concatenated_extractions.py
@@ -15,33 +15,23 @@ from sklearn.decomposition import PCA
 # python test_concatenated_extractions.py --dataset_name esp_fake --attribute text
 
 
-def get_extracted_features_and_labels(args, models, fold_idx, phase='train'):
-        extraction_results = []
-        labels_from_file = []
-        for model_name in models[args.dataset_name]:
-            extraction_result_filename = f'./extracted_features/{args.dataset_name}_{model_name}/fold_{fold_idx}_X_{phase}_{args.attribute}.npy'
-            extraction_result = np.load(extraction_result_filename)
-            extraction_results.append(extraction_result)
-            label_filename = f'./extracted_features/{args.dataset_name}_{model_name}/fold_{fold_idx}_y_{phase}_{args.attribute}.npy'
-            label = np.load(label_filename)
-            labels_from_file.append(label)
-        concatenated_extractions = np.concatenate(extraction_results, axis=1)
-        for i in range(1, len(labels_from_file)):
-            if not np.array_equal(labels_from_file[0], labels_from_file[i]): # all labels should be the same
-                raise Exception(f'labels for {models[args.dataset_name][i]} extractions are different than the others!')
-        return concatenated_extractions, labels_from_file[0]
-
 def main():
     args = parse_args()
     docs, labels = datasets.load_dataset(args.dataset_name, attribute=args.attribute)
-
-    acc_all = []
-
     models = {
         'esp_fake': ('bert_multi', 'beto', 'lda', 'tf_idf'),
         'bs_detector': ('bert_eng', 'bert_multi', 'lda', 'tf_idf'),
         'mixed': ('bert_eng', 'bert_multi', 'beto', 'lda', 'tf_idf'),
     }
+    train_datasets = {
+        'bert_multi': ('esp_fake', 'bs_detector', 'mixed'),
+        'beto': ('esp_fake', 'mixed'),
+        'bert_eng': ('bs_detector', 'mixed'),
+        'lda': ('esp_fake', 'bs_detector', 'mixed'),
+        'tf_idf': ('esp_fake', 'bs_detector', 'mixed'),
+    }
+
+    acc_all = []
 
     k_fold = sklearn.model_selection.RepeatedStratifiedKFold(n_splits=2, n_repeats=5, random_state=42)
     pbar = tqdm(enumerate(k_fold.split(docs, labels)), desc='Fold feature extraction', total=10)
@@ -53,46 +43,120 @@ def main():
             y_test_from_cross_validation[np.argwhere(y_test_from_cross_validation == 2).flatten()] = 0
             y_test_from_cross_validation[np.argwhere(y_test_from_cross_validation == 3).flatten()] = 1
 
-        X_train, y_train = get_extracted_features_and_labels(args, models, fold_idx, 'train')
-        assert(np.array_equal(y_train, y_train_from_cross_validation))
-        X_test, y_test = get_extracted_features_and_labels(args, models, fold_idx, 'test')
-        assert(np.array_equal(y_test, y_test_from_cross_validation))
-        number_of_models = len(models[args.dataset_name])
-        num_features = X_train.shape[1] // number_of_models
+        X_train_all, y_train_all = get_extracted_features_and_labels(args, models, train_datasets, fold_idx, 'train')
+        if args.mode == '4M':
+            assert(np.array_equal(y_train_all[0], y_train_from_cross_validation))
+        X_test_all, y_test_all = get_extracted_features_and_labels(args, models, train_datasets, fold_idx, 'test')
+        if args.mode == '4M':
+            assert(np.array_equal(y_test_all[0], y_test_from_cross_validation))
 
-        if args.feature_selection == 'mutual_info':
-            feature_selector = SelectKBest(score_func=mutual_info_classif, k=num_features)
-            feature_selector.fit(X_train, y_train)
-            selected_X_train = feature_selector.transform(X_train)
-            selected_X_test = feature_selector.transform(X_test)
-        elif args.feature_selection == 'anova':
-            feature_selector = SelectKBest(score_func=f_classif, k=num_features)
-            feature_selector.fit(X_train, y_train)
-            selected_X_train = feature_selector.transform(X_train)
-            selected_X_test = feature_selector.transform(X_test)
-        else:
-            preliminary_pca = PCA(n_components=min(num_features, len(X_train)))
-            preliminary_pca.fit(X_train)
-            # print(pca.explained_variance_ratio_)
-            cumulative = np.cumsum(preliminary_pca.explained_variance_ratio_)
-            indexes = np.argwhere(cumulative > 0.9)
-            num_pca_features = indexes.min() + 1
-            pca = PCA(n_components=num_pca_features)
-            pca.fit(X_train)
-            selected_X_train = pca.transform(X_train)
-            selected_X_test = pca.transform(X_test)
+        for i in range(len(X_train_all)):
+            X_train = X_train_all[i]
+            y_train = y_train_all[i]
+            X_test = X_test_all[i]
+            y_test = y_test_all[i]
 
-        model = sklearn.neural_network.MLPClassifier((500,500))
-        model.fit(selected_X_train, y_train)
+            if args.mode == '4M':
+                num_features = X_train.shape[1] // 4  # average over number of models
+            elif args.mode == '3M':
+                num_features = X_train.shape[1] // 3  # average over number of possible training datasets
+            elif args.mode == '12M':
+                num_features = X_train.shape[1] // 12  # average over both
 
-        y_pred = model.predict(selected_X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        acc_all.append(accuracy)
-        print(f'fold {fold_idx}, accuracy = {accuracy}')
+            selected_X_train, selected_X_test = select_features(args, X_train, y_train, X_test, num_features)
+
+            model = sklearn.neural_network.MLPClassifier((500, 500))
+            model.fit(selected_X_train, y_train)
+
+            y_pred = model.predict(selected_X_test)
+            accuracy = accuracy_score(y_test, y_pred)
+            acc_all.append(accuracy)
+            acc_update = f'fold {fold_idx}, accuracy = {accuracy}' if len(X_train_all) == 1 else f'fold {fold_idx}, model = {models[args.dataset_name][i]}, accuracy = {accuracy}'
+            print(acc_update)
 
     output_path = pathlib.Path('results/')
     os.makedirs(output_path, exist_ok=True)
-    np.save(output_path / f'{args.dataset_name}_concat_extraction_model_avrg_{args.feature_selection}_{args.attribute}.npy', acc_all)
+    np.save(output_path / f'{args.dataset_name}_concat_extraction_model_avrg_{args.feature_selection}_{args.attribute}_{args.mode}.npy', acc_all)
+
+
+def get_extracted_features_and_labels(args, models, train_datasets, fold_idx, phase='train'):
+    if args.mode == '4M':
+        loaded_features = []
+        loaded_labels = []
+        for model_name in models[args.dataset_name]:
+            features_filename = f'./extracted_features/{args.dataset_name}_{model_name}_{args.dataset_name}/fold_{fold_idx}_X_{phase}_{args.attribute}.npy'
+            features = np.load(features_filename)
+            loaded_features.append(features)
+            label_filename = f'./extracted_features/{args.dataset_name}_{model_name}_{args.dataset_name}/fold_{fold_idx}_y_{phase}_{args.attribute}.npy'
+            label = np.load(label_filename)
+            loaded_labels.append(label)
+        loaded_features = np.concatenate(loaded_features, axis=1)
+        feature_list = [loaded_features]
+        for i in range(1, len(loaded_labels)):
+            if not np.array_equal(loaded_labels[0], loaded_labels[i]):  # all labels should be the same
+                raise Exception(f'labels for {models[args.dataset_name][i]} extractions are different than the others!')
+        labels = [loaded_labels[0]]
+    elif args.mode == '3M':
+        feature_list = []
+        labels = []
+        for model_name in models[args.dataset_name]:
+            model_features = []
+            for train_dataset in train_datasets[model_name]:
+                features_filename = f'./extracted_features/{train_dataset}_{model_name}_{args.dataset_name}/fold_{fold_idx}_X_{phase}_{args.attribute}.npy'
+                features = np.load(features_filename)
+                model_features.append(features)
+                if train_dataset == args.dataset_name:
+                    label_filename = f'./extracted_features/{train_dataset}_{model_name}_{args.dataset_name}/fold_{fold_idx}_y_{phase}_{args.attribute}.npy'
+                    label = np.load(label_filename)
+                    labels.append(label)
+            model_features = np.concatenate(model_features, axis=1)
+            feature_list.append(model_features)
+            assert model_features.shape[0] == label.shape[0]
+        assert len(feature_list) == 4
+        assert len(labels) == 4
+    elif args.mode == '12M':
+        feature_list = []
+        labels = []
+        for model_name in models[args.dataset_name]:
+            for train_dataset in train_datasets[model_name]:
+                features_filename = f'./extracted_features/{train_dataset}_{model_name}_{args.dataset_name}/fold_{fold_idx}_X_{phase}_{args.attribute}.npy'
+                features = np.load(features_filename)
+                feature_list.append(features)
+                if train_dataset == args.dataset_name:
+                    label_filename = f'./extracted_features/{train_dataset}_{model_name}_{args.dataset_name}/fold_{fold_idx}_y_{phase}_{args.attribute}.npy'
+                    label = np.load(label_filename)
+                    labels.append(label)
+        feature_list = [np.concatenate(feature_list, axis=1)]
+        for i in range(1, len(labels)):
+            if not np.array_equal(labels[0], labels[i]):  # all labels should be the same
+                raise Exception(f'labels for {models[args.dataset_name][i]} extractions are different than the others!')
+        labels = [labels[0]]
+
+    return feature_list, labels
+
+
+def select_features(args, X_train, y_train, X_test, num_features):
+    if args.feature_selection == 'mutual_info':
+        feature_selector = SelectKBest(score_func=mutual_info_classif, k=num_features)
+        feature_selector.fit(X_train, y_train)
+        selected_X_train = feature_selector.transform(X_train)
+        selected_X_test = feature_selector.transform(X_test)
+    elif args.feature_selection == 'anova':
+        feature_selector = SelectKBest(score_func=f_classif, k=num_features)
+        feature_selector.fit(X_train, y_train)
+        selected_X_train = feature_selector.transform(X_train)
+        selected_X_test = feature_selector.transform(X_test)
+    else:
+        preliminary_pca = PCA(n_components=min(num_features, len(X_train)))
+        preliminary_pca.fit(X_train)
+        cumulative = np.cumsum(preliminary_pca.explained_variance_ratio_)
+        indexes = np.argwhere(cumulative > 0.9)
+        num_pca_features = indexes.min() + 1
+        pca = PCA(n_components=num_pca_features)
+        pca.fit(X_train)
+        selected_X_train = pca.transform(X_train)
+        selected_X_test = pca.transform(X_test)
+    return selected_X_train, selected_X_test
 
 
 def parse_args():
@@ -101,6 +165,7 @@ def parse_args():
     parser.add_argument('--dataset_name', type=str, choices=('esp_fake', 'bs_detector', 'mixed'))
     parser.add_argument('--attribute', choices=('text', 'title'), required=True)
     parser.add_argument('--feature_selection', type=str, choices=('anova', 'mutual_info', 'pca'))
+    parser.add_argument('--mode', type=str, choices=('3M', '4M', '12M'))
 
     args = parser.parse_args()
     return args
