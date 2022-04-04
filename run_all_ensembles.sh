38121c961cdf3055a040a6bac5f63cfd08f531c3
jedrzejkozal <jedrzej.kozal@gmail.com>
2022-04-04 13:55:14 +0200

    update reuslts

--- run_all_ensembles.sh
+++ run_all_ensembles.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
 
-# python test_ensemble.py --dataset_name=esp_fake --attribute=text --mode=3M
-# python test_ensemble.py --dataset_name=esp_fake --attribute=title --mode=3M
-# python test_ensemble.py --dataset_name=bs_detector --attribute=text --mode=3M
-# python test_ensemble.py --dataset_name=bs_detector --attribute=title --mode=3M
-# python test_ensemble.py --dataset_name=mixed --attribute=text --mode=3M
-# python test_ensemble.py --dataset_name=mixed --attribute=title --mode=3M
+python test_ensemble.py --dataset_name=esp_fake --attribute=text --mode=3M
+python test_ensemble.py --dataset_name=esp_fake --attribute=title --mode=3M
+python test_ensemble.py --dataset_name=bs_detector --attribute=text --mode=3M
+python test_ensemble.py --dataset_name=bs_detector --attribute=title --mode=3M
+python test_ensemble.py --dataset_name=mixed --attribute=text --mode=3M
+python test_ensemble.py --dataset_name=mixed --attribute=title --mode=3M
 
-#python test_ensemble.py --dataset_name=esp_fake --attribute=text --mode=4M
-#python test_ensemble.py --dataset_name=esp_fake --attribute=title --mode=4M
-#python test_ensemble.py --dataset_name=bs_detector --attribute=text --mode=4M
-#python test_ensemble.py --dataset_name=bs_detector --attribute=title --mode=4M
-#python test_ensemble.py --dataset_name=mixed --attribute=text --mode=4M
-#python test_ensemble.py --dataset_name=mixed --attribute=title --mode=4M
+python test_ensemble.py --dataset_name=esp_fake --attribute=text --mode=4M
+python test_ensemble.py --dataset_name=esp_fake --attribute=title --mode=4M
+python test_ensemble.py --dataset_name=bs_detector --attribute=text --mode=4M
+python test_ensemble.py --dataset_name=bs_detector --attribute=title --mode=4M
+python test_ensemble.py --dataset_name=mixed --attribute=text --mode=4M
+python test_ensemble.py --dataset_name=mixed --attribute=title --mode=4M
 
 python test_ensemble.py --dataset_name=esp_fake --attribute=text --mode=12M
 python test_ensemble.py --dataset_name=esp_fake --attribute=title --mode=12M
